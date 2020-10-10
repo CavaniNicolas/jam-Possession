45f687884618d85d154862c438831d42f8dcd6ce
Lilian Hiault <lilian.hiault@laposte.net>
2020-10-10 15:18:40 +0200

    Add StatsCharacter.java

---
+++ src/StatsCharacter.java
@@ -0,0 +1,27 @@
+public class StatsCharacter
+{
+    public static int speedMax()
+    {
+	return 100;
+    }
+    public static int speedMin()
+    {
+	return 1;
+    }
+    public static int rangeMax()
+    {
+	return 100;
+    }
+    public static int rangeMin()
+    {
+	return 1;
+    }
+    public static int strenthMin()
+    {
+	return 1;
+    }
+    public static int strenthMax()
+    {
+	return 100;
+    }    
+}
